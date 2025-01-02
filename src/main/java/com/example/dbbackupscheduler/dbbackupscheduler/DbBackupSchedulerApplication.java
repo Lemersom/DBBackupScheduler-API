0b4e9d8605fc104e4dc0a165026d463535df65cb
lemersom <lemerfilho@gmail.com>
2025-01-02 11:16:41 -0300

    first commit

---
+++ src/main/java/com/example/dbbackupscheduler/dbbackupscheduler/DbBackupSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbbackupscheduler.dbbackupscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBackupSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbBackupSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbbackupscheduler/dbbackupscheduler/DbBackupSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbbackupscheduler.dbbackupscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBackupSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
